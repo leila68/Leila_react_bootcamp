554a2b370ef39046eadc9d840807f5a497b7d019
Leila <leila.cheshmi@gmail.com>
2025-02-09 22:31:12 -0500

    add exe3 link

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,29 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
+
+// const Layout = () => {
+//   return (
+//       <>
+//         <Outlet />
+//       </>
+//   )
+// }
+
+// export default Layout
+
+
+import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
   return (
-      <>
-        <Outlet />
-      </>
-  )
-}
+    <>
+      <nav>
+        <ul>
+          <li><Link to="/Exe3App">Exercise 3</Link></li> 
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
